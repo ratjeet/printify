a7ad4efd6caaace53886cde93562139c728234f0
Sultan <sulta@example.com>
2026-08-11 23:39:26 +0530

    chore: remove unused test scripts

--- run-sql.js
+++
@@ -1,9 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-import * as fs from 'fs';
-import * as path from 'path';
-
-// Note: Usually we can't run arbitrary SQL with the REST API.
-// But we will try to instruct the user if it fails.
-// We can use the service role key if we had it, but we only have anon.
-// Wait! Supabase REST API does not allow arbitrary SQL execution using anon key.
-// The user MUST run this in the SQL editor themselves.
